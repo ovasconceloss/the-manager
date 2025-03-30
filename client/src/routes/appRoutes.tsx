2629d5ee560d9305a8eddf7efed67c313948aaee
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-03-30 15:46:39 +0100

    feat: create a file to group together all the application's routes

---
+++ client/src/routes/appRoutes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MenuRoutes from "./menuRoutes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/*" element={<MenuRoutes />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
