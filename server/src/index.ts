1970dde07af82ff75555154eb96e097eb9c2f44b
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-03-30 12:41:48 +0100

    feat: create a function to start the server with fastify

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import fastify from "./fastify";
+
+const startServer = () => {
+    try {
+        fastify.listen({ port: 3000 }, (error, address) => {
+            if (error) console.error(error);
+
+            console.log(`Running at ${address}`);
+        })
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+startServer();
