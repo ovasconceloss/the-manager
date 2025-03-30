932c899e09d0a9f176494bff45c57dc0ab00e389
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-03-30 13:34:13 +0100

    feat: create a function to load a user save

--- server/src/config/saves.ts
+++ server/src/config/saves.ts
@@ -6,13 +6,14 @@ class SaveSystem {
     static createSaveName = (): string => {
         const date = new Date();
     
-        const [year, month, day] = [
+        const [year, month, day, milliseconds] = [
             date.getFullYear(),
             String(date.getMonth() + 1).padStart(2, '0'),
             String(date.getDate()).padStart(2, '0'),
+            String(date.getMilliseconds()).padStart(3, '0'),
         ];
     
-        return `save_${year}-${month}-${day}.tm`;
+        return `save_${year}-${month}-${day}-${milliseconds}.tm`;
     }
     
     static ensureSavesFolder = () => {
@@ -25,7 +26,24 @@ class SaveSystem {
     
             return saveFilePath;
         } catch (error) {
-            console.error(`${error}`);
+            console.error(error);
+            process.exit(1);
+        }
+    }
+
+    static loadSaveFile = (filePath: string): string => {
+        const userDocumentsPath = process.env.DOCUMENTS || path.join(os.homedir(), 'Documents');
+        const saveFilePath = path.resolve(userDocumentsPath, "ProPlay Games", "The Manager 2025", "games",  filePath);
+
+        try {
+            if (fs.existsSync(saveFilePath)) {
+                return saveFilePath;
+            } else {
+                console.error(`Save file not found: ${filePath}`);
+                process.exit(1);
+            }
+        } catch (error) {
+            console.error(error);
             process.exit(1);
         }
     }
