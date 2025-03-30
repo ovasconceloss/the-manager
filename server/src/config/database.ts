fc9883f8c064c5b9bf470733610601c8e50b755d
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-03-30 14:40:03 +0100

    refactor: refactor the database class to make it more organized

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,24 +1,58 @@
+import fs from "fs";
+import path from "path";
 import SaveSystem from "./saves";
 import Database from "better-sqlite3";
 
 class DatabaseSystem {
-    static createDatabase = (): string => { return SaveSystem.ensureSavesFolder(); }
+    static createDatabase = (): Database.Database => {
+        const databasePath = SaveSystem.ensureSavesFolder();
+        const database = new Database(databasePath);
 
-    static loadDatabase = (fileName: string): string => {
-        if (!fileName) throw new Error("File name is required to load a database.");
+        this.executeMigrations(database);
 
-        return SaveSystem.loadSaveFile(fileName);
+        return database;
     }
 
-    static connectDatabase = (action: "load" | "create", fileName?: string) => {
+    static loadDatabase = (fileName: string): Database.Database => {
+        if (!fileName) throw new Error("File name is required to load a database.");
+
+        const databasePath = SaveSystem.loadSaveFile(fileName);
+
+        return new Database(databasePath);
+    };
+
+    static executeMigrations = (database: Database.Database) => {
+        const migrationsPath = path.resolve(__dirname, "../migrations");
+
         try {
-            const databasePath = action === "load" ? DatabaseSystem.loadDatabase(fileName!) : DatabaseSystem.createDatabase();
-            return new Database(databasePath);
+            const migrationFiles = fs.readdirSync(migrationsPath).filter(file => file.endsWith(".sql"));
+
+            for (const file of migrationFiles) {
+                const filePath = path.join(migrationsPath, file);
+                const sql = fs.readFileSync(filePath, "utf-8");
+
+                database.exec(sql);
+            }
         } catch (error) {
             console.error(error);
             process.exit(1);
         }
     }
+
+    static connectDatabase = (action: "load" | "create", fileName?: string): Database.Database => {
+        try {
+            if (action === "load") {
+                return this.loadDatabase(fileName!);
+            } else if (action === "create") {
+                return this.createDatabase();
+            } else {
+                throw new Error("Invalid action. Use 'load' or 'create'.");
+            }
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    };
 }
 
 export default DatabaseSystem;
