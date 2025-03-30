bcc88174c6bd2a21b163b1f7c7c59d2e5e49d41b
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-03-30 15:30:51 +0100

    feat: create a command to run the server together with the client

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -1,5 +1,12 @@
+use std::process::Command;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    Command::new("node")
+        .arg("../../server/dist/index.js")
+        .spawn()
+        .expect("Failed to start server process");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
