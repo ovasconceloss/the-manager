dc1cd08f0ecac81e732a18df8d1a43aab29d8a34
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-01 20:00:46 +0100

    feat: create a new database (save) and insert the graphs into it

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,22 @@
 import fastify from "./fastify";
+import DatabaseSystem from "./config/database";
+import GameImagesGraphics from "./config/graphics";
 
 const startServer = () => {
-    try {
-        fastify.listen({ port: 3000 }, (error, address) => {
-            if (error) console.error(error);
+  try {
+    fastify.listen({ port: 3000 }, (error, address) => {
+      if (error) console.error(error);
 
-            console.log(`Running at ${address}`);
-        });
+      console.log(`Running at ${address}`);
+    });
 
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+    const { database, isNew } = DatabaseSystem.connectDatabase("create");
+    if (isNew) GameImagesGraphics.insertImagesAutomatically(database);
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 startServer();
