3e605941a0e64584e6e1af99b3b2caa5aaa6a0f7
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-01 21:21:42 +0100

    feat: create and register the routes for the static database controller

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
+import staticRoutes from "./routes/static.routes";
 
 const fastify = Fastify();
+
+fastify.register(staticRoutes);
 
 export default fastify;
---
+++ server/src/routes/static.routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from "fastify";
+import StaticController from "../controllers/static.controller";
+
+
+export default async function staticRoutes(fastify: FastifyInstance) {
+  fastify.get("/static/club/all/:nation", StaticController.getClubsByNation);
+}
