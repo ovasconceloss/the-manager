9283e711b55d08b21b59e72ec888d6be8085526a
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-01 20:46:48 +0100

    feat: create a method to initialize the static database as soon as the server is started

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,21 @@ import fastify from "./fastify";
 import DatabaseSystem from "./config/database";
 import GameImagesGraphics from "./config/graphics";
 
-const startServer = () => {
+const initializeDatabase = async () => {
+  try {
+    const staticDatabaseExist = await DatabaseSystem.checkStaticDatabaseExists();
+
+    if (!staticDatabaseExist) {
+      const staticDatabase = DatabaseSystem.createStaticDatabase();
+      await GameImagesGraphics.insertImagesAutomatically(staticDatabase);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+const startServer = async () => {
   try {
     fastify.listen({ port: 3000 }, (error, address) => {
       if (error) console.error(error);
@@ -10,9 +24,7 @@ const startServer = () => {
       console.log(`Running at ${address}`);
     });
 
-    const { database, isNew } = DatabaseSystem.connectDatabase("create");
-    if (isNew) GameImagesGraphics.insertImagesAutomatically(database);
-
+    await initializeDatabase();
   } catch (error) {
     console.error(error);
     process.exit(1);
