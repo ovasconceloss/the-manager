4ab6458848e30c5887407c8c71e55ce13861a146
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-01 20:32:43 +0100

    feat: create the functions to generate the static database
    - create function to generate static database
    - create function to run migrations from the static database

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,9 +1,29 @@
 import fs from "fs";
+import os from "os";
 import path from "path";
 import SaveSystem from "./saves";
 import Database from "better-sqlite3";
 
 class DatabaseSystem {
+    static createStaticDatabase = (): Database.Database => {
+        const userConfigPath = process.env.APPDATA || path.join(os.homedir());
+        const databasePath = path.resolve(userConfigPath, "The Manager 2025", "TM2025_STATIC.db");
+
+        try {
+            const databaseFolderPath = path.dirname(databasePath);
+            if (!fs.existsSync(databaseFolderPath)) fs.mkdirSync(databaseFolderPath, { recursive: true });
+
+            const database = new Database(databasePath);
+
+            this.executeStaticMigrations(database);
+
+            return database;
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    }
+
     static createDatabase = (): Database.Database => {
         const databasePath = SaveSystem.ensureSavesFolder();
         const database = new Database(databasePath);
@@ -20,6 +40,24 @@ class DatabaseSystem {
 
         return new Database(databasePath);
     };
+
+    static executeStaticMigrations = (database: Database.Database) => {
+        const migrationsPath = path.resolve(__dirname, "../migrations/static");
+
+        try {
+            const migrationFiles = fs.readdirSync(migrationsPath).filter(file => file.endsWith(".sql"));
+
+            for (const file of migrationFiles) {
+                const filePath = path.join(migrationsPath, file);
+                const sql = fs.readFileSync(filePath, "utf-8");
+
+                database.exec(sql);
+            }
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    }
 
     static executeMigrations = (database: Database.Database) => {
         const migrationsPath = path.resolve(__dirname, "../migrations");
