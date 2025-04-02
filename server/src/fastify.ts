d20210082da9bc2719ae2749da2c4220e6c4d22c
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-02 21:16:06 +0100

    feat: setting up cors on fastify

--- server/src/fastify.ts
+++ server/src/fastify.ts
@@ -1,7 +1,15 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import staticRoutes from "./routes/static.routes";
 
 const fastify = Fastify();
+
+fastify.register(cors, {
+    origin: ["*", "http://localhost:1420", "tauri://localhost"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+});
 
 fastify.register(staticRoutes);
 
