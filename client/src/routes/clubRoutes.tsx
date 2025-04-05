bec9a57a4aba5c137ff7b7fa2f8b3d7327575aff
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-05 21:34:24 +0100

    feat: create a route to the manager creation page

--- client/src/routes/clubRoutes.tsx
+++ client/src/routes/clubRoutes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ChooseClub from "@/pages/club/clubChoose";
 import { Routes, Route } from "react-router-dom";
+import CreateManager from "@/pages/manager/createManager";
 import { ProfileProvider } from "@/context/ProfileContext";
 
 const ClubRoutes: React.FC = () => {
@@ -8,6 +9,7 @@ const ClubRoutes: React.FC = () => {
     <ProfileProvider>
       <Routes>
         <Route path="/choose" element={<ChooseClub />} />
+        <Route path="/create/manager" element={<CreateManager />} />
       </Routes>
     </ProfileProvider>
   );
