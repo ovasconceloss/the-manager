6167333b2f8bded5f9caa439276a426eee278625
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-04-05 15:13:45 +0100

    feat: group the club's routes in the profile provider

--- client/src/routes/clubRoutes.tsx
+++ client/src/routes/clubRoutes.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ChooseClub from "@/pages/club/clubChoose";
 import { Routes, Route } from "react-router-dom";
+import { ProfileProvider } from "@/context/ProfileContext";
 
 const ClubRoutes: React.FC = () => {
   return (
+    <ProfileProvider>
       <Routes>
         <Route path="/choose" element={<ChooseClub />} />
       </Routes>
+    </ProfileProvider>
   );
 };
 
