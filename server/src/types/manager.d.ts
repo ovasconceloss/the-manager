9d2015879970343bb764718639c1c94e7b9359eb
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-20 21:13:03 +0100

    feat: create a type (for typing) of a manager

---
+++ server/src/types/manager.d.ts
@@ -0,0 +1,10 @@
+export type Manager = {
+    first_name: string,
+    last_name: string,
+    birth_date: string,
+    matches: number,
+    wins: number,
+    draws: number,
+    losses: number,
+    nation_id: number
+}
