818c8f078612e85d92c558cf0853e4bb3c833860
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-22 21:41:35 +0100

    feat: create a function in the service to create the database (save)

--- server/src/services/static.service.ts
+++ server/src/services/static.service.ts
@@ -1,8 +1,15 @@
+import DatabaseSystem from "../config/database";
 import StaticModel from "../models/static.model";
+import { setDatabaseInstance } from "../config/databaseInstance";
 
 class StaticService {
     static async fetchClubsByNation(nation: string) {
         return await StaticModel.getClubsByNation(nation);
+    }
+
+    static async createSaveDatabase() {
+        const database = DatabaseSystem.connectDatabase("create");
+        setDatabaseInstance(database);
     }
 }
 
