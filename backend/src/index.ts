d13404545b3da62f80662d3393d40c93e57a2382
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-27 20:15:52 +0100

    feat: create a function to start the server on port 8080

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import fastify from "./fastify";
+
+const startServer = async () => {
+    try {
+        await fastify.listen({ port: 8080 });
+        fastify.log.info(`Server running on port 8080`);
+    } catch (err) {
+        fastify.log.error(`Failed to initialize the server: `, err);
+        process.exit(1);
+    }
+}
+
+process.on('unhandledRejection', (reason) => {
+    console.error("Unhandled Rejection:", reason);
+    process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception:", err);
+    process.exit(1);
+});
+
+startServer();
