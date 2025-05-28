aff536ccbf6ae71adcb1b09bffca85d2969dd078
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-28 19:17:31 +0100

    feat: configure fastify cors for connection to the front end

--- backend/src/fastify.ts
+++ backend/src/fastify.ts
@@ -1,8 +1,19 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import saveRoutes from "./routes/saveRoutes";
 import { errorHandlerPlugin } from "./plugins/error-handler";
 
 const fastify = Fastify({ logger: true });
 
 errorHandlerPlugin(fastify);
+
+fastify.register(cors, {
+    origin: ["*", "http://localhost:1420", "tauri://localhost"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+});
+
+fastify.register(saveRoutes);
 
 export default fastify;
