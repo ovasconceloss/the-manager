3705b04d61e07d819c6f40be8e227d4a3b9c5232
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-29 22:19:31 +0100

    refactor: use await to resolve the promise of the current season

--- backend/src/controllers/seasonController.ts
+++ backend/src/controllers/seasonController.ts
@@ -2,16 +2,16 @@ import SeasonService from "../services/seasonService";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 class SeasonController {
-    static getCurrentSeason(request: FastifyRequest, reply: FastifyReply) {
+    static async getCurrentSeason(request: FastifyRequest, reply: FastifyReply) {
         try {
-            const seasonInformation = SeasonService.insertSeason();
+            const seasonInformation = await SeasonService.fetchCurrentSeason();
             return reply.status(201).send({ seasonInformation });
         } catch (err) {
             return reply.status(500).send({ error: err, message: "Failed to retrieve data for the current season" });
         }
     }
 
-    static createSeason(request: FastifyRequest, reply: FastifyReply) {
+    static async createSeason(request: FastifyRequest, reply: FastifyReply) {
         try {
             SeasonService.insertSeason();
             return reply.status(201).send({ message: "New season successfully started." });
