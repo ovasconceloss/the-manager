c18082b00a7080fd4ddd3e5d7a5518b26bc33341
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-29 22:20:07 +0100

    feat: create a function to search for the club by id

--- backend/src/models/clubModel.ts
+++ backend/src/models/clubModel.ts
@@ -1,6 +1,24 @@
 import SessionSystem from "../core/session/session";
 
 class ClubModel {
+    static async getClubById(clubId: number) {
+        const databaseInstance = SessionSystem.getDatabase();
+
+        const sql = `
+            SELECT
+                club.*,
+                league.id,
+                nation.id,
+                nation.name as nation_name
+            FROM club
+            LEFT JOIN league ON league.id = club.league_id
+            LEFT JOIN nation ON nation.id = club.nation_id
+            WHERE club.id = ?
+        `;
+
+        return databaseInstance.prepare(sql).get(clubId);
+    }
+
     static async getClubByLeague(leagueId: number) {
         const databaseInstance = SessionSystem.getDatabase();
 
