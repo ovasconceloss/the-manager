aa607a1a04a3eb120a28a8e0788ce75451f91587
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-30 22:28:58 +0100

    fix: correct the seed service to remove the player_nation table

--- backend/src/scripts/databaseSeed.ts
+++ backend/src/scripts/databaseSeed.ts
@@ -4,15 +4,14 @@ import { CLUBS_BY_NATION } from "../data/clubs";
 import SeedService from "../services/seedService";
 import { getFakerByNation, randomValues } from "../utils/utils";
 
-const databaseInstance = new Database("./src/core/database/default/default.db");
+const databaseInstance = new Database("./src/core/database/default/default.tm");
 
 databaseInstance.transaction(() => {
     const nationStatement = databaseInstance.prepare("INSERT INTO nation (name, continent, reputation) VALUES (?, ?, ?)");
     const leagueStatement = databaseInstance.prepare("INSERT INTO league (name, nation_id, reputation) VALUES (?, ?, ?)");
     const clubStatement = databaseInstance.prepare("INSERT INTO club (name, abbreviation, city, stadium, nation_id, league_id, reputation) VALUES (?, ?, ?, ?, ?, ?, ?)");
-    const playerStatement = databaseInstance.prepare("INSERT INTO player (first_name, last_name, birth_date, position, overall, potential, market_value) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    const playerStatement = databaseInstance.prepare("INSERT INTO player (nation_id, first_name, last_name, birth_date, position, overall, potential, market_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
     const contractStatement = databaseInstance.prepare("INSERT INTO player_contract (player_id, club_id, start_date, end_date, salary) VALUES (?, ?, ?, ?, ?)");
-    const playerNationStatement = databaseInstance.prepare("INSERT INTO player_nation (player_id, nation_id) VALUES (?, ?)");
     const staffStatement = databaseInstance.prepare("INSERT INTO staff (first_name, last_name, birth_date, function, tactical_style) VALUES (?, ?, ?, ?, ?)");
     const staffContractStatement = databaseInstance.prepare("INSERT INTO staff_contract (staff_id, club_id, start_date, end_date, salary) VALUES (?, ?, ?, ?, ?)");
     const staffNationStatement = databaseInstance.prepare("INSERT INTO staff_nation (staff_id, nation_id) VALUES (?, ?)");
@@ -34,7 +33,7 @@ databaseInstance.transaction(() => {
             const clubId = SeedService.seedClub(clubStatement, clubData, nationId, leagueId, clubReputation);
 
             SeedService.seedStaff(staffStatement, staffNationStatement, staffContractStatement, faker, nationId, clubId);
-            SeedService.seedPlayers(playerStatement, contractStatement, playerNationStatement, faker, clubId, nationId);
+            SeedService.seedPlayers(playerStatement, contractStatement, faker, clubId, nationId);
         }
     }
 })();
