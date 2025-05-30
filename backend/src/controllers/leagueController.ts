78c977537f674d61467aac4808ab242c3112b9bd
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-30 21:48:17 +0100

    feat: create the league controller functions - get all leagues - get a league by id

--- backend/src/controllers/leagueController.ts
+++ backend/src/controllers/leagueController.ts
@@ -1,5 +1,26 @@
-class LeagueController {
+import LeagueService from "../services/leagueService";
+import { FastifyReply, FastifyRequest } from "fastify";
 
+class LeagueController {
+    static async getAllLeagues(request: FastifyRequest, reply: FastifyReply) {
+        try {
+            const leagues = await LeagueService.fetchAllLeagues();
+            return reply.status(200).send({ leagues: leagues });
+        } catch (err) {
+            return reply.status(500).send({ error: err, message: "Failed to retrieve data from all leagues" });
+        }
+    }
+
+    static async getLeagueById(request: FastifyRequest<{ Params: { leagueId: number } }>, reply: FastifyReply) {
+        const leagueId = request.params.leagueId;
+
+        try {
+            const league = await LeagueService.fetchLeagueById(leagueId);
+            return reply.status(200).send({ league: league });
+        } catch (err) {
+            return reply.status(500).send({ error: err, message: "Failed to retrieve data from league by ID" });
+        }
+    }
 }
 
 export default LeagueController;
