1a25e86fc0c2bc052228b9b502a6c497a7092306
ovasconceloss <victorolimpiovasconcelos.dev@gmail.com>
2025-05-31 13:06:12 +0100

    feat: remove the entire time passage system

--- backend/src/services/seasonService.ts
+++ backend/src/services/seasonService.ts
@@ -1,7 +1,4 @@
 import SeasonModel from "../models/seasonModel";
-import CalendarService from "../core/simulation/calendarService";
-import GameLoopService from "../core/simulation/gameLoopService";
-import GameStateService from "../core/simulation/gameStateService";
 
 class SeasonService {
     static async fetchCurrentSeason() {
@@ -11,15 +8,7 @@ class SeasonService {
     static async insertSeason() {
         const seasonId = await SeasonModel.createSeason();
 
-        CalendarService.createLeagueFixtures(seasonId);
-        GameStateService.initializeGameState(seasonId);
-
         return seasonId;
-    }
-
-    static async advanceDay() {
-        GameLoopService.advanceDay();
-        return true;
     }
 }
 
